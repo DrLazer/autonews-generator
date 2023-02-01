7f08345813ca177b894638abdd0bab6310e46dd1
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2023-02-01 23:39:04 +0000

    db batch write

--- generator/db-write.js
+++ generator/db-write.js
@@ -1,15 +1,40 @@
 'use strict';
 
-const { v4: uuidv4 } = require('uuid');
+const { DynamoDB } = require("aws-sdk");
+const db = new DynamoDB.DocumentClient()
+const TableName = process.env.SOURCE_META_TABLE
 
 module.exports.dynamo = async (event) => {
   if (!(Array.isArray(event) && event.length)) {
     return 'Input needs to be an array of objects';
   }
 
-  let params = {
-    RequestItems: {
-     "SourceMeta": event
-    }
-  };
+  // Build the batches
+  var batches = [];
+  var current_batch = [];
+  var item_count = 0;
+  for (var x in event) {
+      // Add the item to the current batch
+      item_count++;
+      current_batch.push(data[x]);
+      // If we've added 25 items, add the current batch to the batches array
+      // and reset it
+      if(item_count%25 == 0) {
+          batches.push(current_batch);
+          current_batch = [];
+      }
+  }
+  // Add the last batch if it has records and is not equal to 25
+  if (current_batch.length > 0 && current_batch.length != 25) batches.push(current_batch);
+
+  for (x in batches) {
+    let params = {
+      RequestItems: {
+        "SourceMeta": event
+      }
+    };
+    // Perform the batchWrite operation
+    db.batchWrite(params, handler(params));
+  }
+
 };
