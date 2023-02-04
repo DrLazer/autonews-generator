191af04c9a3af6557ca81ab02df39a8c18795d6f
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2023-02-04 14:50:51 +0000

    step function yaml

--- generator/db-transform.js
+++ generator/db-transform.js
@@ -3,12 +3,12 @@
 const { v4: uuidv4 } = require('uuid');
 
 module.exports.dynamo = async (event) => {
-  if (!(Array.isArray(event) && event.length)) {
+  if (!(Array.isArray(event.body) && event.body.length)) {
     return 'Input needs to be an array of objects';
   }
 
   const items = [];
-  event.forEach(element => {
+  event.body.forEach(element => {
     let dynamoElements = {};
     Object.keys(element).forEach(key => {
       dynamoElements[key] = element[key];
