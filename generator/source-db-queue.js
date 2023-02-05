eab7fbc2265d5dda2085b737ebe75baa73adf865
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2023-02-05 14:26:51 +0000

    push to queue code

--- generator/source-db-queue.js
+++ generator/source-db-queue.js
@@ -1,9 +1,28 @@
 'use strict';
 
-const { DynamoDB } = require("aws-sdk");
-const db = new DynamoDB.DocumentClient()
-const TableName = process.env.SOURCE_META_TABLE
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS({
+    apiVersion: 'latest',
+    region: process.env.AWS_REGION,
+});
 
 module.exports.push = async (event) => {
-  console.log(event);
+  console.log(JSON.stringify(event));
+  if (!event?.Records) {
+    console.log('Event needs to be dynamo stream output');
+    return;
+  }
+  
+  for (record in event.Records) {
+    if (record.eventName !== 'INSERT') {
+      console.log('Not an insert, not adding to queue');
+    }
+    if (record.eventName === 'INSERT') {
+      await sqs.sendMessage({
+        QueueUrl: process.env.QUEUE_URL,
+        // Any message data we want to send
+        MessageBody: record.dynamodb.NewImage
+      }).promise(); 
+    }
+  });
 };
